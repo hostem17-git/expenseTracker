bbc1f4b8568d316f4f852b37d8751a2b92fdbd44
Vinayak Jaiswal <58824030+hostem17-git@users.noreply.github.com>
2025-01-30 18:01:40 +0530

    twilio webhook middleware added

--- routes/whatsapp.route.js
+++ routes/whatsapp.route.js
@@ -3,9 +3,15 @@ import { whatsAppMiddleWare } from "../middleware/whatsapp.middleware.js";
 import { AddExpense } from "../controllers/whatsapp.controller.js";
 import twilio from "twilio";
 
+// const temp = (req,res,next)=>{
+
+//     const temp = ;
+//     temp(req,res,next);
+//     next();
+// }
 
 const router = Router();
 
-router.post("/",twilio.webhook(),whatsAppMiddleWare,AddExpense);
+router.post("/",whatsAppMiddleWare,twilio.webhook(),AddExpense);
 
 export default router;
