d89ed1fe0e6634b9e3ea3108991702e130f34ce5
Vinayak Jaiswal <58824030+hostem17-git@users.noreply.github.com>
2025-01-30 20:02:51 +0530

    moved webhook url to environment variable

--- middleware/whatsapp.middleware.js
+++ middleware/whatsapp.middleware.js
@@ -3,8 +3,7 @@ import twilio from "twilio";
 export const whatsAppMiddleWare = async (req, res, next) => {
   try {
     const twilioSignature = req.headers["x-twilio-signature"];
-    const webhookUrl =
-      "https://ae6d-103-3-204-186.ngrok-free.app/api/v1/whatsapp";
+    const webhookUrl = process.env.TWILIO_WEBHOOK_URL;
     const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
     const isValid = twilio.validateRequest(
       TWILIO_AUTH_TOKEN,
