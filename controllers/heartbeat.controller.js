1fd37dc11e9c7af465ecf001ed7d1756d926a192
hostem17 <vjjais17@gmail.com>
2025-02-05 13:40:06 +0530

    heartbeat route reponse shortened

--- controllers/heartbeat.controller.js
+++ controllers/heartbeat.controller.js
@@ -1,7 +1,6 @@
 export const heartbeat = async (req, res) => {
     console.log("Heartbeat route hit");
-    res.status(200).json({
-        success: true,
-        message: "Server is running",
+    return res.status(200).json({
+        message: "Ok"
     });
 }
